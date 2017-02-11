3e943b7365053f7a710bc9ca21833f2e36890cfb
cocoadavid <szilard.david91@gmail.com>
2017-02-11 20:14:18 +0100

    Update init_database.py

--- init_database.py
+++ init_database.py
@@ -4,5 +4,5 @@ from models import *
 class InitDatabase:
 
     def init_db():
-        ConnectDatabase.db.drop_tables([UserStory], safe=True, cascade=True)
+        ConnectDatabase.db.drop_tables([UserStory], safe=True)
         ConnectDatabase.db.create_tables([UserStory], safe=True)
